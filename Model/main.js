ab500643c6d2eb8547e4a683b81305ae34c2f6ce
Patrick Meyer <tbt_paddik@hotmail.com>
2018-04-10 13:34:08 +0200

    tester

---
+++ Model/main.js
@@ -0,0 +1,66 @@
+const puppeteer = require('puppeteer');
+const CREDS = require('../creds');
+
+// DOM element selectors
+const USERNAME_SELECTOR = '#login_field';
+const PASSWORD_SELECTOR = '#password';
+const BUTTON_LOGIN_SELECTOR = '#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block';
+const LIST_USERNAME_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > a > em';
+const LIST_EMAIL_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > ul > li:nth-child(2) > a';
+
+const LENGTH_SELECTOR_CLASS = 'user-list-class';
+
+async function run() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://github.com/login');
+
+    // Interact with web page
+    await page.click(USERNAME_SELECTOR);
+    await page.keyboard.type(CREDS.username);
+
+    await page.click(PASSWORD_SELECTOR);
+    await page.keyboard.type(CREDS.password);
+
+    await page.click(BUTTON_LOGIN_SELECTOR);
+
+    await page.waitForNavigation();
+
+    // Search Github
+    const userToSearch = 'john';
+    const searchUrl = 'https://github.com/search?q=' + userToSearch +'&type=Users&utf8=%E2%9C%93';
+
+    await page.goto(searchUrl);
+    await page.waitFor(2*1000);
+
+    // Fetching Names and Emails:
+    let listLength = await page.evaluate((sel) => {
+        return document.getElementsByClassName(sel).length;
+    }, LENGTH_SELECTOR_CLASS);
+
+    for (let i = 1; i <= listLength; i++) {
+        // change index to next child:
+        let usernameSelector = LIST_USERNAME_SELECTOR.replace("INDEX", i);
+        let emailSelector = LIST_EMAIL_SELECTOR.replace("INDEX", i);
+
+        let username = await page.evaluate((sel) => {
+            return document.querySelector(sel).getAttribute('href').replace('/','');
+        }, usernameSelector);
+
+        let email = await page.evaluate((sel) => {
+            let element = document.querySelector(sel);
+            return element? element.innerHTML: null;
+        }, emailSelector);
+
+        // not all users have emails visible
+        if (!email)
+            continue;
+
+        console.log(username, ' -> ', email);
+    }
+
+
+}
+
+run();
